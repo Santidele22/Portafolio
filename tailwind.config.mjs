6cab2cd7d1777b0c0f4f68ac8ac6f705882e3ed9
Santiago Delebecq <santiidele21@gmail.com>
2023-12-01 22:51:04 -0300

    V1 Finishid

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        color1: "#522AB8",
+        color2: "#4E3985",
+        color3: "#4806EA",
+        color4: "#3C3352",
+        color5: "#2D2A33",
+      },
+    },
+  },
+  plugins: [],
+};
