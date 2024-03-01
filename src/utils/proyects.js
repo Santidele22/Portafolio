d4f71d0ff6a5f47d2f5aeee99d99c17db4492d97
Santiago Delebecq <santiidele21@gmail.com>
2024-03-01 19:18:25 -0300

    Incomplete: add bento grid

---
+++ src/utils/proyects.js
@@ -0,0 +1,68 @@
+//image
+import dogImage from "../assets/image/dog.png";
+import rickImage from "../assets/image/rickandmorty.png";
+import videogameImage from "../assets/image/videogame.png";
+import mountain from "../assets/image/mountain.png";
+import Aegir from "../assets/image/aegir-chat.png";
+export const proyects = [
+  {
+    imageUrl: mountain,
+    title: "Mountain Haven",
+    tech: [
+      "./icons/react.svg",
+      "./icons/tailwind.svg",
+      "./icons/express-.svg",
+      "./icons/cloudinary.svg",
+      "./icons/mongodb.svg",
+    ],
+    projectLinks: "https://github.com/NicoBalbuena/ProyectoMountain",
+  },
+  {
+    imageUrl: dogImage,
+    title: "Landing Dog",
+    tech: [
+      "./icons/react.svg",
+      "./icons/css.svg",
+      "./icons/express-.svg",
+      "./icons/sequelize.svg",
+      "./icons/postgresql.svg",
+    ],
+    projectLinks: "https://github.com/Santidele22/landing-dogs",
+  },
+  {
+    imageUrl: videogameImage,
+    title: "Landing Videogame",
+    tech: [
+      "./icons/react.svg",
+      "./icons/css.svg",
+      "./icons/express-.svg",
+      "./icons/sequelize.svg",
+      "./icons/postgresql.svg",
+    ],
+    projectLinks: "https://github.com/Santidele22",
+  },
+  {
+    title: "Aegir chat",
+    projectLinks: "https://github.com/Santidele22/AegirChat",
+    imageUrl: Aegir,
+    tech: [
+      "./icons/react.svg",
+      "./icons/css.svg",
+      "./icons/express-.svg",
+      "./icons/sequelize.svg",
+      "./icons/postgresql.svg",
+    ],
+  },
+  {
+    imageUrl: rickImage,
+    title: "Landing RickandMorty",
+    tech: [
+      "./icons/react.svg",
+      "./icons/css.svg",
+      "./icons/express-.svg",
+      "./icons/sequelize.svg",
+      "./icons/postgresql.svg",
+    ],
+    projectLinks: "https://github.com/pi-rym/PI-Santidele22",
+  },
+];
